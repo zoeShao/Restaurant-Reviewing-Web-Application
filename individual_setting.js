5ab52b86610063a3befadb52970c5c9b2eb80373
Yuting Shao <yuting.shao@mail.utoronto.ca>
2019-03-03 21:43:39 -0500

    Updated individual pages

---
+++ individual_setting.js
@@ -0,0 +1,94 @@
+/* Full patrons entries element */
+const infoForm = document.querySelector('#infoForm')
+
+/* Event listeners for button submit and button click */
+infoForm.addEventListener('click', modifyUserInfo);
+
+
+function modifyUserInfo(e) {
+	e.preventDefault();
+	if (e.target.classList.contains('editInfo')) {
+		if (e.target.innerText === 'edit') {
+			console.log(e.target.parentElement.parentElement.parentElement)
+			e.target.parentElement.style.position = "static"
+			addProfileSelector(e.target.parentElement.parentElement.parentElement)
+			addTaskTextBox(e.target.parentElement.firstElementChild)
+			addTaskTextBox(e.target.parentElement.firstElementChild.nextElementSibling)
+			addPassTaskTextBox(e.target.parentElement.firstElementChild.nextElementSibling.nextElementSibling)
+			// change the button text to 'save'
+			e.target.style.left = "94%";
+			e.target.style.top = "100%"
+			e.target.innerText = 'save'
+		} else {
+			e.target.parentElement.style.position = "relative"
+			e.target.parentElement.style.top = "10%";
+			removeProfileSelector(e.target.parentElement.parentElement.parentElement)
+			removeTaskTextBox(e.target.parentElement.firstElementChild)
+			removeTaskTextBox(e.target.parentElement.firstElementChild.nextElementSibling)
+			removePassTaskTextBox(e.target.parentElement.firstElementChild.nextElementSibling.nextElementSibling)
+			// change the button text to 'edit'
+			e.target.style.top = "75%";
+			e.target.style.left = "0%";
+			e.target.innerText = 'edit'
+		}
+	}
+}
+
+function addTaskTextBox(task) {
+	const spanElement = task.firstElementChild.nextElementSibling
+	const taskText = spanElement.innerText;
+	task.removeChild(spanElement)
+	const textBox = document.createElement('input')
+	textBox.type = 'text'
+	textBox.className = "form-control"
+	textBox.value = taskText;
+	task.appendChild(textBox)
+}
+
+function removeTaskTextBox(task) {
+	const taskLabel = document.createElement('span')
+	taskLabel.className = "info"
+	const textBox = task.lastElementChild
+	const taskText = document.createTextNode(textBox.value)
+	taskLabel.appendChild(taskText)
+	textBox.before(taskLabel)
+	task.removeChild(textBox)
+}
+
+function addProfileSelector(task) {
+	const divElement = document.createElement('div')
+	divElement.className = "file-loading"
+	const labelElement = document.createElement('label')
+	labelElement.htmlFor = "avatar"
+	labelElement.innerHTML = "<strong>Choose a profile picture:</strong>"
+	const inputElement = document.createElement('input')
+	inputElement.type = 'text'
+	inputElement.id = "avatar"
+	inputElement.type = "file"
+	inputElement.name = "avatar"
+	inputElement.accept ="image/*"
+	divElement.appendChild(labelElement)
+	divElement.appendChild(inputElement)
+	task.firstElementChild.firstElementChild.nextElementSibling.appendChild(divElement)
+	console.log(task.firstElementChild.firstElementChild.nextElementSibling)
+}
+
+function removeProfileSelector(task) {
+	const fileLoadingDiv = task.firstElementChild.firstElementChild.nextElementSibling.firstElementChild
+	task.firstElementChild.firstElementChild.nextElementSibling.removeChild(fileLoadingDiv)
+}
+
+function addPassTaskTextBox(task) {
+	task.firstElementChild.innerText = "Password: "
+	const textBox = document.createElement('input')
+	textBox.type = 'text'
+	textBox.className = "form-control"
+	textBox.value = "user";
+	task.appendChild(textBox)
+}
+
+function removePassTaskTextBox(task) {
+	task.firstElementChild.innerText = ""
+	const textBox = task.lastElementChild
+	task.removeChild(textBox)
+}
