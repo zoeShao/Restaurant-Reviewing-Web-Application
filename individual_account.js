bc5c29eb3d98872b85cfbbe59173cbb8bd5f1dad
Yuting Shao <yuting.shao@mail.utoronto.ca>
2019-03-04 02:29:53 -0500

    Updated individual account pages

---
+++ individual_account.js
@@ -0,0 +1,145 @@
+class User {
+	constructor(image, name, email, password, type){
+        this.image = image;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+        this.reviews = [];
+    }
+}
+
+class Review {
+	constructor(rName, uName, rate, price, content){
+        this.rName = rName;
+        this.uName = uName;
+        this.rate = rate;
+        this.price = price;
+        this.content = content;
+    }
+}
+
+const userImg = document.createElement('img')
+userImg.src = "avatar.jpg"
+userImg.alt = "avatar Picture";
+const user = new User(userImg, "user", "user@mail.com", "user", "i")
+const review1 = new Review("McDonald's", "user", 4, 1, "here is the review.here is the review.here is the review.here is the review.")
+const review2 = new Review("Osaka Sushi", "user", 3, 2, "here is the review.here is the review.")
+// const review3 = new Review("Asaka Sushi", "user", 5, 3, "here is the review.")
+user.reviews.push(review2)
+user.reviews.push(review1)
+// user.reviews.push(review3)
+console.log(user)
+
+
+/* Full patrons entries element */
+const dropDown = document.querySelector('#dropDown')
+const contentBody = document.querySelector('#mainBody')
+
+/* Event listeners for button submit and button click */
+dropDown.addEventListener('click', sortTheItem);
+
+function sortTheItem(e) {
+	e.preventDefault();
+	if (e.target.classList.contains('dropdown-name')) {
+		contentBody.innerText = ""
+		sortByName(user)
+		for (let i = 0; i < user.reviews.length; i++) {
+			addReviewToDom(user.reviews[i])
+		}
+	} else if (e.target.classList.contains('dropdown-rate')) {
+		contentBody.innerText = ""
+		sortByRate(user)
+		for (let i = 0; i < user.reviews.length; i++) {
+			addReviewToDom(user.reviews[i])
+		}
+	}
+	else if (e.target.classList.contains('dropdown-price')) {
+		contentBody.innerText = ""
+		sortByPrice(user)
+		for (let i = 0; i < user.reviews.length; i++) {
+			addReviewToDom(user.reviews[i])
+		}
+	}
+}
+
+function addReviewToDom(review) {
+	const contentBoxElement = document.createElement('div')
+	contentBoxElement.className = "contentBox"
+	const aElement = document.createElement('a')
+	aElement.className = "reviewLink"
+	aElement.href = "#"
+	const reviewElement = document.createElement('div')
+	reviewElement.className = "storeContainer"
+	const nameElement = document.createElement('p')
+	nameElement.innerHTML = "<strong>Restaurant name: </strong>" + `${review.rName}`
+	const rateElement = addRateToDom(review.rate)
+	const priceElement = addPriceToDom(review.price)
+	const contentElement = document.createElement('p')
+	contentElement.innerHTML = "<strong>Review: </strong>" + `${review.content}`
+	reviewElement.appendChild(nameElement)
+	reviewElement.appendChild(rateElement)
+	reviewElement.appendChild(priceElement)
+	reviewElement.appendChild(contentElement)
+	aElement.appendChild(reviewElement)
+	contentBoxElement.appendChild(aElement)
+	// contentBoxElement.appendChild(reviewElement)
+	console.log(contentBoxElement)
+	contentBody.appendChild(contentBoxElement)
+}
+
+function sortByName(user) {
+	user.reviews.sort(function(a, b){
+    if(a.rName < b.rName) { return -1; }
+    if(a.rName > b.rName) { return 1; }
+    return 0;
+	})
+}
+
+function sortByRate(user) {
+	user.reviews.sort(function(a, b){
+    if(a.rate < b.rate) { return -1; }
+    if(a.rate > b.rate) { return 1; }
+    return 0;
+	})
+}
+
+function sortByPrice(user) {
+	user.reviews.sort(function(a, b){
+    if(a.price < b.price) { return -1; }
+    if(a.price > b.price) { return 1; }
+    return 0;
+	})
+}
+
+function addRateToDom(rate) {
+	const paraElement = document.createElement('p')
+	const strongElement = document.createElement('strong')
+	strongElement.innerText = "Rate: "
+	paraElement.appendChild(strongElement)
+	const linkElement = document.createElement('link')
+	linkElement.rel = "stylesheet"
+	linkElement.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+	paraElement.appendChild(linkElement)
+	for (let i = 0; i < rate; i++) {
+		const spanElement = document.createElement('span')
+		spanElement.className = "fa fa-star checked"
+		paraElement.appendChild(spanElement)
+	}
+	for (let i = rate; i < 5; i++) {
+		const spanElement = document.createElement('span')
+		spanElement.className = "fa fa-star"
+		paraElement.appendChild(spanElement)
+	}
+	return paraElement
+}
+
+function addPriceToDom(rate) {
+	const paraElement = document.createElement('p')
+	let priceRate = ''
+	for (let i = 0; i < rate; i++) {
+		priceRate = priceRate + '$'
+	}
+	paraElement.innerHTML = '<strong>Price: </strong>' + priceRate
+	return paraElement
+}
