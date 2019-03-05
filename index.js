dfe9f2cfed77ceb548e8e41516dc94c1ec85f751
LawZhou <32020799+LawZhou@users.noreply.github.com>
2019-03-05 00:57:16 -0500

    add links to files

--- index.js
+++ index.js
@@ -1,54 +1,64 @@
 
 const log = console.log;
 document.getElementById("dropdownMarkham").addEventListener("click", showMarkhamRestaurants);
-
+document.getElementById("dropdownToronto").addEventListener("click", showDowntownRestaurants);
+//hardcode
+const restaurants = document.getElementById("popularRestaurants");
+changeRestaurant(restaurants.children[0], "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
+                "McDonald's");
+document.getElementById("searchButtonLink").href = "restaurants_search_result.html";
 function showMarkhamRestaurants(e) {
   if (e.target.classList.contains("dropdown-item")){
     const restaurants = document.getElementById("popularRestaurants");
-
-    //   <div class="col-lg-5 col-md-6 mb-4 mr-5">
-    //   <div class="card h-100">
-    //     <a href="#"><img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg"></a>
-    //     <div class="card-body">
-    //       <h4 class="card-title">
-    //         <a href="#">McDonald's</a>
-    //       </h4>
-    //     </div>
-    //     <!-- <div class="card-footer">
-    //       <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-    //     </div> -->
-    //   </div>
-    // </div>
-      const dropDownButton = document.getElementById("dropDownButton");
-      log(dropDownButton);
-      dropDownButton.innerText = "Markham";
-      
-      for (let i = 0; i < restaurants.children.length; i++){
-          const restaurant = restaurants.children[i]
-          
-          //change link
-          const link = restaurant.children[0].children[0];
-        //   link.herf = 
-          log(link);
-          //change image source
-          const img = restaurant.children[0].children[0].children[0];
-          img.src = "http://markhamosakasushi.ca/wp-content/uploads/osaka-front.jpg"
+    const dropDownButton = document.getElementById("dropDownButton");
+    dropDownButton.innerText = "Markham";
     
-          //change name
-          const cardBody = restaurant.children[0].children[1];
-          cardBody.removeChild(cardBody.children[0]);
-          const h4 = document.createElement("h4");
-          h4.className = "card-title";
-          const name = document.createElement("a")
-          name.href = "#";
-          name.appendChild(document.createTextNode("Osaka Sushi"));
-          h4.appendChild(name);
-          cardBody.appendChild(h4);
-          log(cardBody);
-      }
+    for (let i = 0; i < restaurants.children.length; i++){
+        const restaurant = restaurants.children[i]
+        //the restaurant should get from the server
+        changeRestaurant(restaurant, "#", "http://markhamosakasushi.ca/wp-content/uploads/osaka-front.jpg",
+                "Osaka Sushi");
+    }
   }
- 
-
-
-
 }
+
+function showDowntownRestaurants(e) {
+  if (e.target.classList.contains("dropdown-item")){
+    const restaurants = document.getElementById("popularRestaurants");
+    const dropDownButton = document.getElementById("dropDownButton");
+    dropDownButton.innerText = "Downtown-Toronto";
+    
+    for (let i = 0; i < restaurants.children.length; i++){
+        const restaurant = restaurants.children[i]
+        //the restaurant should get from the server
+        changeRestaurant(restaurant, "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
+                "McDonald's");
+    }
+  }
+}
+
+//helper function
+function changeRestaurant(restaurant, link, imgSrc, resName){
+  //change link
+  const aElement = restaurant.children[0].children[0];
+  aElement.href = link;
+  //change image source
+  const img = restaurant.children[0].children[0].children[0];
+  img.src = imgSrc
+
+  //change name
+  const cardBody = restaurant.children[0].children[1];
+  cardBody.removeChild(cardBody.children[0]);
+  const h4 = document.createElement("h4");
+  h4.className = "card-title";
+  const name = document.createElement("a")
+  name.href = link;
+  name.appendChild(document.createTextNode(resName));
+  h4.appendChild(name);
+  cardBody.appendChild(h4);
+  log(cardBody);
+}
+
+
+
+
--- login.js
+++ login.js
@@ -8,10 +8,15 @@ function checkIdentity(e){
     console.log("here");
     e.preventDefault();
     const username = document.querySelector('#usernameInput').value;
-    const password = document.querySelector('#pwInput').value; 
+    const password = document.querySelector('#pwInput').value;
+    //should check username/password with the server 
     if(username == 'user' && password == 'user'){
-        window.location.href = "index.html";
+        window.location.href = "index_user.html";
     } else if(username == 'admin' && password == 'admin'){
         window.location.href = "individual_account_adminView_banUser.html";
+    } else if(username == 'user2' && password == 'user2'){
+        window.location.href = "restaurant_account.html";
+    } else{
+        alert("Wrong username/password");
     }
 }
