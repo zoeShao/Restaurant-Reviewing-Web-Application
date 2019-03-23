3f26cb7a02d840523e34d81fefc45ec8bd7acb9e
Lawrence <lawrence@Lawrences-MacBook-Pro.local>
2019-03-23 17:13:05 -0400

    change front end look

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ document.getElementById("dropdownMarkham").addEventListener("click", showMarkham
 document.getElementById("dropdownToronto").addEventListener("click", showDowntownRestaurants);
 
 //server part
-const express = require('express');
-const router = express.Router();
-const User = require('user');
+// const express = require('express');
+// const router = express.Router();
+// const User = require('user');
 
 //hardcode
 const restaurants = document.getElementById("popularRestaurants");
 changeRestaurant(restaurants.children[0], "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
                 "McDonald's");
 document.getElementById("searchButtonLink").href = "restaurants_search_result.html";
+
+// addPopularRestaurant();
+// changeRestaurant(restaurants.children[1], "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
+//                 "McDonald's");
+
+// addPopularRestaurant();
+// changeRestaurant(restaurants.children[2], "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
+//                 "McDonald's");
+
 function showMarkhamRestaurants(e) {
   if (e.target.classList.contains("dropdown-item")){
     const restaurants = document.getElementById("popularRestaurants");
@@ -65,20 +74,40 @@ function changeRestaurant(restaurant, link, imgSrc, resName){
   log(cardBody);
 }
 
+function addPopularRestaurant(){
+  const colDiv = document.createElement('div');
+  colDiv.className = "col-lg-5 col-md-6 mb-4 mr-5";
+  const card = document.createElement('div');
+  card.className = "card h-100";
+  const link = document.createElement('a');
+  const img = document.createElement('img');
+  img.className = "card-img-top";
+  link.appendChild(img);
+  const cardBody = document.createElement('div');
+  cardBody.className = "card-body";
+  const h4Title = document.createElement('h4');
+  h4Title.className = "card-title";
+  h4Title.appendChild(document.createElement('a'));
+  cardBody.appendChild(h4Title);
+  card.appendChild(link)
+  card.appendChild(cardBody);
+  colDiv.appendChild(card);
+  restaurants.appendChild(colDiv);
+}
 
 //server 
 
-router.get('/', function(req, res){
-  if(!req.session.user){ //user or User?
-    return res.status(401).send();
-  }
+// router.get('/', function(req, res){
+//   if(!req.session.user){ //user or User?
+//     return res.status(401).send();
+//   }
 
-  return res.status(200).send("logged In");
-})
+//   return res.status(200).send("logged In");
+// })
 
 
-router.get('/', function(req, res){
-  req.session.destroy();
+// router.get('/', function(req, res){
+//   req.session.destroy();
 
-  return res.status(200).send();
-})
+//   return res.status(200).send();
+// })
