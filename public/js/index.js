f6b921a558be5292324c8108a1ff79a9fc61c012
Lawrence <lawrence@Lawrences-MacBook-Pro.local>
2019-03-30 13:58:56 -0400

    finishi popular restaurants

--- public/js/index.js
+++ public/js/index.js
@@ -4,22 +4,15 @@ const log = console.log;
 document.getElementById("dropdownMarkham").addEventListener("click", showMarkhamRestaurants);
 document.getElementById("dropdownToronto").addEventListener("click", showDowntownRestaurants);
 
+
+let maxShowingRestaurant = 2;
+const popularRestaurantsElement = document.getElementById("popularRestaurants");
+
+//initialize calls
 getLogInInfo();
+InitializePopularRestaurants("Downtown", maxShowingRestaurant);
 
-//hardcode
-const restaurants = document.getElementById("popularRestaurants");
-changeRestaurant(restaurants.children[0], "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
-                "McDonald's");
-document.getElementById("searchButtonLink").href = "restaurants_search_result.html";
-
-// addPopularRestaurant();
-// changeRestaurant(restaurants.children[1], "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
-//                 "McDonald's");
-
-// addPopularRestaurant();
-// changeRestaurant(restaurants.children[2], "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
-//                 "McDonald's");
-
+/***************Get data from server***************************/
 //send request to server to get log in status
 function getLogInInfo(){
   const url = '/getLogInInfo';
@@ -70,6 +63,26 @@ function signOutUser(){
     })
     }
 }
+
+//Get list of popular restaurant
+function getListOfPopularRestaurants(location){
+	const url = '/popularRestaurants';
+    // The data we are going to send in our request
+    let data = {
+        location: location
+    }
+    // Create our request constructor with all the parameters we need
+    const request = new Request(url, {
+        method: 'post', 
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+    });
+    return fetch(request)
+}
+/************************************************/
 
 
 function changeToSignOutStatus(){
@@ -142,16 +155,11 @@ function changeToLoggedInStatus(userName, imgSrc){
 
 function showMarkhamRestaurants(e) {
   if (e.target.classList.contains("dropdown-item")){
-    const restaurants = document.getElementById("popularRestaurants");
+    //change drop down button text
     const dropDownButton = document.getElementById("dropDownButton");
     dropDownButton.innerText = "Markham";
-    
-    for (let i = 0; i < restaurants.children.length; i++){
-        const restaurant = restaurants.children[i]
-        //Server part TODO: the restaurant should get from the server
-        changeRestaurant(restaurant, "#", "http://markhamosakasushi.ca/wp-content/uploads/osaka-front.jpg",
-                "Osaka Sushi");
-    }
+    //get data from server 
+    InitializePopularRestaurants("Markham", maxShowingRestaurant)
   }
 }
 
@@ -161,35 +169,41 @@ function showDowntownRestaurants(e) {
     const dropDownButton = document.getElementById("dropDownButton");
     dropDownButton.innerText = "Downtown-Toronto";
     
-    for (let i = 0; i < restaurants.children.length; i++){
-        const restaurant = restaurants.children[i]
-        //Server part TODO: the restaurant should get from the server
-        changeRestaurant(restaurant, "review_page.html", "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg",
-                "McDonald's");
-    }
+    InitializePopularRestaurants("Downtown", maxShowingRestaurant)
   }
 }
 
+function InitializePopularRestaurants(location, maxShowingRestaurant){
+  getListOfPopularRestaurants(location).then((res) => {
+    return res.json();
+  }).then((resList) => {
+    for (let i = 0; i < maxShowingRestaurant && i < resList.length; i++){
+      if(popularRestaurantsElement.children.length < maxShowingRestaurant){
+          addPopularRestaurant();
+      }
+      changeRestaurant(popularRestaurantsElement.children[i], resList[i]);
+    }
+  }).catch(error => {log(error);});
+   
+}
+
 //helper function
-function changeRestaurant(restaurant, link, imgSrc, resName){
+function changeRestaurant(restaurant, resObj){
   //change link
-  const aElement = restaurant.children[0].children[0];
-  aElement.href = link;
+  const form = restaurant.children[0].children[0]
+  form.action = "";
+  const hiddenInput = form.children[0]
+  hiddenInput.value = resObj._id;
+
   //change image source
-  const img = restaurant.children[0].children[0].children[0];
-  img.src = imgSrc
+  const img = form.children[1].children[0];
+  img.src = "https://upload.wikimedia.org/wikipedia/commons/4/4b/McDonald%27s_logo.svg";//resObj.picture;
 
   //change name
-  const cardBody = restaurant.children[0].children[1];
-  cardBody.removeChild(cardBody.children[0]);
-  const h4 = document.createElement("h4");
-  h4.className = "card-title";
-  const name = document.createElement("a")
-  name.href = link;
-  name.appendChild(document.createTextNode(resName));
-  h4.appendChild(name);
-  cardBody.appendChild(h4);
-  log(cardBody);
+  const cardBody = form.children[2];
+  const h4 = cardBody.children[0]
+  const btnLink = h4.children[0];
+  btnLink.innerText = resObj.name;
 }
 
 function addPopularRestaurant(){
@@ -197,20 +211,40 @@ function addPopularRestaurant(){
   colDiv.className = "col-lg-5 col-md-6 mb-4 mr-5";
   const card = document.createElement('div');
   card.className = "card h-100";
-  const link = document.createElement('a');
+
+  const form = document.createElement('form');
+  form.method = "post";
+
+  const hiddenInput = document.createElement("input");
+  hiddenInput.type = "hidden"
+  form.appendChild(hiddenInput);
+
+  const link = document.createElement('button');
+  link.className = "resTitleLink";
+  link.type = "submit";
+
   const img = document.createElement('img');
   img.className = "card-img-top";
   link.appendChild(img);
+  form.appendChild(link);
+
   const cardBody = document.createElement('div');
   cardBody.className = "card-body";
+
   const h4Title = document.createElement('h4');
   h4Title.className = "card-title";
-  h4Title.appendChild(document.createElement('a'));
+
+  const titleLink = document.createElement('button')
+  titleLink.className = "resTitleLink";
+  titleLink.type = "submit";
+
+  h4Title.appendChild(titleLink);
   cardBody.appendChild(h4Title);
-  card.appendChild(link)
-  card.appendChild(cardBody);
+  
+  form.appendChild(cardBody);
+  card.appendChild(form)
   colDiv.appendChild(card);
-  restaurants.appendChild(colDiv);
+  popularRestaurantsElement.appendChild(colDiv);
 }
 
 
