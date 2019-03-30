85788c1ab96e07c6841a4391d1b2edb27318695a
Lawrence <lawrence@Lawrences-MacBook-Pro.local>
2019-03-29 21:24:07 -0400

    link home page to log in and sign out page

--- public/js/login.js
+++ public/js/login.js
@@ -45,19 +45,19 @@ console.log(loginForm);
 //     })
 // })
 
-function checkIdentity(e){
-    console.log("here");
-    e.preventDefault();
-    const username = document.querySelector('#usernameInput').value;
-    const password = document.querySelector('#pwInput').value;
-    //server part TODO: should check username/password with the server 
-    if(username == 'user' && password == 'user'){
-        window.location.href = "index_user.html";
-    } else if(username == 'admin' && password == 'admin'){
-        window.location.href = "individual_account_adminView_banUser.html";
-    } else if(username == 'user2' && password == 'user2'){
-        window.location.href = "restaurant_account.html";
-    } else{
-        alert("Wrong username/password");
-    }
-}
+// function checkIdentity(e){
+//     console.log("here");
+//     e.preventDefault();
+//     const username = document.querySelector('#usernameInput').value;
+//     const password = document.querySelector('#pwInput').value;
+//     //server part TODO: should check username/password with the server 
+//     if(username == 'user' && password == 'user'){
+//         window.location.href = "index_user.html";
+//     } else if(username == 'admin' && password == 'admin'){
+//         window.location.href = "individual_account_adminView_banUser.html";
+//     } else if(username == 'user2' && password == 'user2'){
+//         window.location.href = "restaurant_account.html";
+//     } else{
+//         alert("Wrong username/password");
+//     }
+// }
